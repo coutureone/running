cc3b664a02823274d8f6b1a3a08f0223e15103d3
couture <couturecome@gmail.com>
2025-05-09 09:01:44 +0800

    add code

--- src/components/YearsStat/index.tsx
+++ src/components/YearsStat/index.tsx
@@ -1,6 +1,7 @@
 import YearStat from '@/components/YearStat';
 import useActivities from '@/hooks/useActivities';
 import { INFO_MESSAGE } from '@/utils/const';
+import React from "react";
 
 const YearsStat = ({ year, onClick }: { year: string, onClick: (_year: string) => void }) => {
   const { years } = useActivities();
@@ -12,25 +13,35 @@ const YearsStat = ({ year, onClick }: { year: string, onClick: (_year: string) =
 
   // for short solution need to refactor
   return (
-    <div className="w-full lg:w-full pb-16 pr-16 lg:pr-16">
+    <div className="w-full pb-16 pr-16 lg:w-full lg:pr-16">
       <section className="pb-0">
         <p className="leading-relaxed">
           {INFO_MESSAGE(years.length, year)}
           <br />
-        </p>
+          <br />
+          春风若有怜花意，可否许我再少年。
+          </p>
+          <p  style={quoteStyle}>&ndash;&ndash;《咏苍髯》</p>
       </section>
       <hr color="red" />
       {yearsArrayUpdate.map((year) => (
         <YearStat key={year} year={year} onClick={onClick} />
       ))}
-      {// eslint-disable-next-line no-prototype-builtins
+      {
+        // eslint-disable-next-line no-prototype-builtins
         yearsArrayUpdate.hasOwnProperty('Total') ? (
           <YearStat key="Total" year="Total" onClick={onClick} />
         ) : (
           <div />
-        )}
+        )
+      }
     </div>
   );
+};
+
+const quoteStyle: React.CSSProperties= {
+    fontWeight:"bold",
+    textAlign: "right"
 };
 
 export default YearsStat;
