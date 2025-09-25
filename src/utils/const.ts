8f3bee231823f4043a35fbc82f8ae65e72a5b9f5
couture <couturecome@gmail.com>
2025-09-25 21:07:41 +0800

    map

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ const MAPBOX_TOKEN =
   // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
   // Instead, manually add a new token and apply URL restrictions.
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJjbWYxdXR4YncwMTJtMm5zOTE4eTZpMGdtIn0.OnsXdwkZFztR8a5Ph_T-xg';
+  'sk.eyJ1Ijoic2V0ZmRyZCIsImEiOiJjbWZ6ZXc0dDcwMTloMmlzOW5tNnZwODUyIn0.pnpPAVJKxt2VCpz5AsmGCw';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
