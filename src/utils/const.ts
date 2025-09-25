d2e72709b613c5bb51b54d361fac827d5de1db29
couture <couturecome@gmail.com>
2025-09-25 15:11:00 +0800

    add code

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -38,7 +38,7 @@ const LINE_OPACITY = 0.4;
 // Use smaller height on mobile devices for better user experience
 const MAP_HEIGHT = window.innerWidth <= 768 ? 250 : 600;
 //set to `false` if you want to hide the road label characters
-const ROAD_LABEL_DISPLAY = false;
+const ROAD_LABEL_DISPLAY = true;
 // updated on 2024/11/17: privacy mode is set to true by default
 //set to `true` if you want to display only the routes without showing the map.
 const PRIVACY_MODE = false;
@@ -46,9 +46,9 @@ const PRIVACY_MODE = false;
 //set to `false` if you want to make light off as default, only effect when `PRIVACY_MODE` = false
 const LIGHTS_ON = false;
 //set to `true` if you want to show the 'Elevation Gain' column
-const SHOW_ELEVATION_GAIN = true;
+const SHOW_ELEVATION_GAIN = false;
 // richer title for the activity types (like garmin style)
-const RICH_TITLE = true;
+const RICH_TITLE = false;
 
 // IF you are outside China please make sure IS_CHINESE = false
 const IS_CHINESE = true;
