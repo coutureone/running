80b579cca6c836b07291b5f1c233678a86421558
GitHub Action <action@github.com>
2026-04-19 06:40:52 +0000

    update new runs

--- src/components/YearsStat/index.tsx
+++ src/components/YearsStat/index.tsx
@@ -6,11 +6,9 @@ import { INFO_MESSAGE } from '@/utils/const';
 const YearsStat = ({
   year,
   onClick,
-  onYearSummaryClick,
 }: {
   year: string;
   onClick: (_year: string) => void;
-  onYearSummaryClick?: (_year: string) => void;
 }) => {
   const { years } = useActivities();
 
@@ -35,21 +33,11 @@ const YearsStat = ({
         <p className="leading-relaxed">
           {infoMessage}
           <br />
-          <br />
-          何为强大？何为奔跑？何为生存？一切都在风之彼方。希望就在我们各自心中。看啊，道路就在我们脚下。所以我们要奔跑于今天。一直奔跑，直到永恒。
-        </p>
-        <p className="text-right" style={{ fontFamily: 'serif' }}>
-          ——《强风吹拂》
         </p>
       </section>
       <hr />
       {yearsArrayUpdate.map((yearItem) => (
-        <YearStat
-          key={yearItem}
-          year={yearItem}
-          onClick={onClick}
-          onYearSummaryClick={onYearSummaryClick}
-        />
+        <YearStat key={yearItem} year={yearItem} onClick={onClick} />
       ))}
     </div>
   );
