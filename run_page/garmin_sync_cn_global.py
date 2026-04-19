8b661677c64874bd56148d88c22aef3622c03bdb
couture <couturecome@gmail.com>
2026-04-19 14:36:43 +0800

    fix: remove unused imports to pass ruff check

--- run_page/garmin_sync_cn_global.py
+++ run_page/garmin_sync_cn_global.py
@@ -9,11 +9,9 @@ import os
 import sys
 
 
-from config import FIT_FOLDER, GPX_FOLDER, JSON_FILE, SQL_FILE
+from config import FIT_FOLDER, GPX_FOLDER
 from garmin_sync import Garmin, get_downloaded_ids
 from garmin_sync import download_new_activities
-from utils import make_activities_file
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
